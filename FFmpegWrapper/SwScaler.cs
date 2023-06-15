4e12260561c30176508fc83989719fe527d7d46f
dubiousconst282 <87553666+dubiousconst282@users.noreply.github.com>
2023-06-14 21:25:26 -0300

    Add missing SwScaler.Convert span overloads

--- FFmpegWrapper/SwScaler.cs
+++ FFmpegWrapper/SwScaler.cs
@@ -29,29 +29,62 @@ public unsafe class SwScaler : FFObject
     }
     public void Convert(AVFrame* src, AVFrame* dst)
     {
-        var srcFmt = InputFormat;
-        var dstFmt = OutputFormat;
-
-        if ((src->format != (int)srcFmt.PixelFormat || src->width != srcFmt.Width || src->height != srcFmt.Height) ||
-            (dst->format != (int)dstFmt.PixelFormat || dst->width != dstFmt.Width || dst->height != dstFmt.Height)
-        ) {
-            throw new ArgumentException("Frame must match rescaler formats");
-        }
-        ffmpeg.sws_scale(Handle, src->data, src->linesize, 0, InputFormat.Height, dst->data, dst->linesize);
+        CheckFrame(src, InputFormat, input: true);
+        CheckFrame(dst, OutputFormat, input: false);
+        ffmpeg.sws_scale(Handle, src->data, src->linesize, 0, src->height, dst->data, dst->linesize);
     }
-    /// <summary> Converts and rescale interleaved pixel data into the destination format.</summary>
+
+    /// <summary> Converts and rescales <paramref name="src"/> into the given frame. The input pixel format must be interleaved. </summary>
+    /// <param name="stride"> The number of bytes per pixel line in <paramref name="src"/>. </param>
     public void Convert(ReadOnlySpan<byte> src, int stride, VideoFrame dst)
     {
-        var srcFmt = InputFormat;
-        var dstFmt = OutputFormat;
+        CheckBuffer(src, stride, InputFormat, input: true);
+        CheckFrame(dst.Handle, OutputFormat, input: false);
 
-        if ((srcFmt.IsPlanar || src.Length < srcFmt.Height * stride) ||
-            (dst.PixelFormat != dstFmt.PixelFormat || dst.Width != dstFmt.Width || dst.Height != dstFmt.Height)
-        ) {
-            throw new ArgumentException("Frame must match rescaler formats");
-        }
         fixed (byte* pSrc = src) {
             ffmpeg.sws_scale(Handle, new[] { pSrc }, new[] { stride }, 0, dst.Height, dst.Handle->data, dst.Handle->linesize);
+        }
+    }
+
+    /// <summary> Converts and rescales <paramref name="src"/> into the given buffer. The output pixel format must be interleaved. </summary>
+    /// <param name="stride"> The number of bytes per pixel line in <paramref name="dst"/>. </param>
+    public void Convert(VideoFrame src, Span<byte> dst, int stride)
+    {
+        CheckFrame(src.Handle, InputFormat, input: true);
+        CheckBuffer(dst, stride, OutputFormat, input: false);
+        
+        fixed (byte* pDst = dst) {
+            ffmpeg.sws_scale(Handle, src.Handle->data, src.Handle->linesize, 0, src.Height, new[] { pDst }, new[] { stride });
+        }
+    }
+
+    /// <summary> Converts and rescales <paramref name="src"/> into the given buffer. The input and output pixel formats must be interleaved. </summary>
+    /// <param name="srcStride"> The number of bytes per pixel line in <paramref name="src"/>. </param>
+    /// <param name="dstStride"> The number of bytes per pixel line in <paramref name="dst"/>. </param>
+    public void Convert(ReadOnlySpan<byte> src, int srcStride, Span<byte> dst, int dstStride)
+    {
+        CheckBuffer(src, srcStride, InputFormat, input: true);
+        CheckBuffer(dst, dstStride, OutputFormat, input: false);
+
+        fixed (byte* pSrc = src)
+        fixed (byte* pDst = dst) {
+            ffmpeg.sws_scale(Handle, new[] { pSrc }, new[] { srcStride }, 0, InputFormat.Height, new[] { pDst }, new[] { dstStride });
+        }
+    }
+
+    private static void CheckFrame(AVFrame* frame, in PictureFormat format, bool input)
+    {
+        if (frame->format != (int)format.PixelFormat || frame->width != format.Width || frame->height != format.Height) {
+            throw new ArgumentException((input ? "Input" : "Output") + " frame must match rescaler format");
+        }
+    }
+
+    private static void CheckBuffer(ReadOnlySpan<byte> buffer, int stride, in PictureFormat format, bool input)
+    {
+        if (format.IsPlanar || buffer.Length < (long)format.Height * stride || 
+            stride < ffmpeg.av_image_get_linesize(format.PixelFormat, format.Width, 0)
+        ) {
+            throw new ArgumentException((input ? "Input" : "Output") + " buffer must match rescaler format");
         }
     }
 
