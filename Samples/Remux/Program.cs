f62acaeae4d2e87fb98be285c2e84b528d77a383
RyanYANG <yangran.cn@gmail.com>
2024-05-07 10:55:22 +0800

    add StreamCopy to MediaMuxer.AddStream; add Remux sample (#7)
    * add StreamCopy to MediaMuxer.AddStream; add Remux sample
    
    * add MediaPacket.BytePosition
    
    ---------
    
    Co-authored-by: Ryan <yangran@kingfar.cn>

---
+++ Samples/Remux/Program.cs
@@ -0,0 +1,26 @@
+﻿// https://github.com/FFmpeg/FFmpeg/blob/master/doc/examples/remux.c
+
+using FFmpeg.Wrapper;
+
+if (args.Length < 2) {
+    Console.WriteLine("Usage: Remux <input path> <output path.mkv>");
+    return;
+}
+
+using var demuxer = new MediaDemuxer(args[0]);
+using var remuxer = new MediaMuxer(args[1]);
+
+var srcStreams = demuxer.Streams.OrderBy(s => s.Index).ToArray();
+var dstStreams = new MediaStream[srcStreams.Length];
+foreach (var srcStream in srcStreams) {
+    dstStreams[srcStream.Index] = remuxer.AddStream(srcStream);
+}
+remuxer.Open();
+
+using var packet = new MediaPacket();
+while (demuxer.Read(packet)) {
+    var index = packet.StreamIndex;
+    packet.RescaleTS(srcStreams[index].TimeBase, dstStreams[index].TimeBase);
+    packet.BytePosition = -1;
+    remuxer.Write(packet);
+}
